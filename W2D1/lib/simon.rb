001e811c5b85e06ce803781174f940f149c0ba16
Ronil Bhatia <ronilbhatia@gmail.com>
2018-06-24 16:55:47 -0700

    Fill out skeleton of Simon

--- W2D1/lib/simon.rb
+++ W2D1/lib/simon.rb
@@ -4,38 +4,71 @@ class Simon
   attr_accessor :sequence_length, :game_over, :seq
 
   def initialize
-
+    @sequence_length = 1
+    @game_over = false
+    @seq = []
   end
 
   def play
-
+    until @game_over
+      take_turn
+    end
+    game_over_message
+    reset_game
   end
 
   def take_turn
-
+    show_sequence
+    require_sequence
+    if @game_over
+      game_over_message
+      reset_game
+    else
+      round_success_message
+      @sequence_length += 1
+    end
   end
 
   def show_sequence
-
+    add_random_color
+    puts "The sequence is the following"
+    @seq.each do |color|
+      puts color
+      sleep(2)
+    end
+    system("clear")
   end
 
   def require_sequence
-
+    puts "Please enter the sequence, with each color separated by a space (e.g. Red Blue Yellow)"
+    user_seq = gets.chomp.split.map(&:downcase)
+    unless user_seq == @seq
+      @game_over = true
+    end
   end
 
   def add_random_color
-
+    @seq << COLORS.sample
   end
 
   def round_success_message
-
+    puts "Nice job - that was the correct sequence. Get ready for the next round"
+    sleep(2)
+    system("clear")
   end
 
   def game_over_message
-
+    puts "Oh no, you messed up!"
   end
 
   def reset_game
-
+    @sequence_length = 1
+    @game_over = false
+    @seq = []
   end
+end
+
+if __FILE__ == $PROGRAM_NAME
+  game = Simon.new
+  game.play
 end
