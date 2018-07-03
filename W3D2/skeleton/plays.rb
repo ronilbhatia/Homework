7f8da82e5350638442c3380e28f1eb945c3116d1
Ronil Bhatia <ronilbhatia@gmail.com>
2018-07-03 09:25:18 -0700

    Finish up homework

--- W3D2/skeleton/plays.rb
+++ W3D2/skeleton/plays.rb
@@ -50,7 +50,7 @@ class Play
   end
 
   def self.find_by_title(title)
-    play = PlayDBConnection.instance.execute(<<-SQL, @title)
+    play = PlayDBConnection.instance.execute(<<-SQL, title)
       SELECT
         *
       FROM
@@ -64,7 +64,79 @@ class Play
   end
 
   def self.find_by_playwright(name)
-    playwright = Playwright.find_by_name(name)
-    raise "#{name} is not in database"
+    playwright_id = Playwright.find_by_name(name)
+    raise "#{name} is not in database" unless playwright.length > 0
+
+    plays = PlayDBConnection.instance.execute(<<-SQL, playwright_id)
+      SELECT
+        *
+      FROM
+        plays
+      WHERE
+        playwright_id = ?
+    SQL
+  end
+
+  class Playwright
+    attr_accessor :name
+
+    def initialize(options)
+      @id = options['id']
+      @name = name['name']
+    end
+
+    def self.all
+      PlayDBConnection.instance.execute(<<-SQL)
+        SELECT
+          *
+        FROM
+          playwrights
+      SQL
+    end
+
+    def self.find_by_name(name)
+      playwright_id = PlayDBConnection.instance.execute(<<-SQL, name)
+        SELECT
+          id
+        FROM
+          playwrights
+        WHERE
+          name = ?
+      SQL
+
+      raise 'Playwright does not exist' unless playwright_id.length > 0
+
+      playwright_id.first
+    end
+
+    def create
+      raise "#{self} already in database" if @id
+
+      PlayDBConnection.instance.execute(<<-SQL, @name)
+        INSERT INTO
+          playwrights (name)
+        VALUES
+          (?)
+      SQL
+
+      @id = PlayDBConnection.instance.last_insert_row_id
+    end
+
+    def update
+      raise "#{self} not in database" unless @id
+
+      PlayDBConnection.instance.execute(<<-SQL, @name, @id)
+        UPDATE
+          playwrights
+        SET
+          name = ?
+        WHERE
+          id = ?
+      SQL
+    end
+
+    def get_plays
+      Play.find_by_playwright(@name)
+    end
   end
 end
