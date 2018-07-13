531214c79c0cef3edb4c5c822d4b84348d0b44e1
Ronil Bhatia <ronilbhatia@gmail.com>
2018-07-12 22:08:56 -0700

    Copy Music App to W4D5 Homework Repo

--- W3D3_new/app/models/person.rb
+++ W3D3_new/app/models/person.rb
@@ -1,8 +1,8 @@
-class Person < ActiveRecord::Base
+class Person < ApplicationRecord
     validates :name, presence: true
     belongs_to :house, {
       primary_key: :id,
       foreign_key: :house_id,
-      class_name: 'House'
+      class_name: :House
     }
 end
